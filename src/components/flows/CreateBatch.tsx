45e3391bd21c27fa97864632bf0c758b4f25b650
goblincore <withallmyhrt@gmail.com>
2023-08-09 16:46:49 -0400

    use better way of overwriting default create view via config

--- src/components/flows/CreateBatch.tsx
+++ src/components/flows/CreateBatch.tsx
@@ -4,18 +4,19 @@ import '../global.scss';
 
 import { DefaultTemplate } from "payload/components/templates";
 import { AdminView } from "payload/config";
+import  formatFields from 'payload/dist/admin/components/views/collections/Edit/formatFields';
 
 const CreateBatch: React.FC = () => {
 
     return  (
-        <DefaultTemplate>
+        <section>
             <section>
 
         <h2> Create New Batch</h2>
 
         </section>
 
-        </DefaultTemplate>
+        </section>
     )
 }
 
