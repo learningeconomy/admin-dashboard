e0aaf4af1686cca287df5585eb4e5a3ab934862b
goblincore <withallmyhrt@gmail.com>
2023-08-09 14:40:49 -0400

    Create Batch use default admin template

--- src/components/flows/CreateBatch.tsx
+++ src/components/flows/CreateBatch.tsx
@@ -2,13 +2,21 @@ import React from "react"
 import './batch.scss';
 import '../global.scss';
 
+import { DefaultTemplate } from "payload/components/templates";
+import { AdminView } from "payload/config";
+
 const CreateBatch: React.FC = () => {
 
-    return  (<div>
+    return  (
+        <DefaultTemplate>
+            <section>
 
         <h2> Create New Batch</h2>
 
-    </div>)
+        </section>
+
+        </DefaultTemplate>
+    )
 }
 
 export default CreateBatch;
