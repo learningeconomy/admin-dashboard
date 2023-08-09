516e48dc66502094e440bbc7f023fadd245bb432
goblincore <withallmyhrt@gmail.com>
2023-08-09 17:44:28 -0400

    Add OperationContext provider around custom create template flow

--- src/components/template/CreateTemplate.tsx
+++ src/components/template/CreateTemplate.tsx
@@ -1,14 +1,54 @@
 import React from "react"
 import { Props } from "../types";
 
+import { OperationContext } from 'payload/dist/admin/components/utilities/OperationProvider'
+
 const CreateTemplate: React.FC = ( props: Props) => {
 
+    const {
+        collection,
+        isEditing,
+        data,
+        onSave: onSaveFromProps,
+        permissions,
+        isLoading,
+        internalState,
+        apiURL,
+        action,
+        hasSavePermission,
+        disableEyebrow,
+        disableActions,
+        disableLeaveWithoutSaving,
+        customHeader,
+        id,
+        updatedAt,
+      } = props;
+
+      const {
+        slug,
+        fields,
+        admin: {
+          useAsTitle,
+          disableDuplicate,
+          preview,
+          hideAPIURL,
+        },
+        versions,
+        timestamps,
+        auth,
+        upload,
+      } = collection;
+
+    const operation = isEditing ? 'update' : 'create';
+
     return (
+        <OperationContext.Provider value={operation}>
         <section>
             <h1>Create Template Flow</h1>
 
 
         </section>
+        </OperationContext.Provider>
 
     );
 }
