536f98718217e4377aa5d29c1e0f293a2dfd58c5
goblincore <withallmyhrt@gmail.com>
2023-08-10 14:45:17 -0400

    add mapping object for fields to steps

--- src/components/batch/RenderBatchFlowFields.tsx
+++ src/components/batch/RenderBatchFlowFields.tsx
@@ -13,6 +13,12 @@ const intersectionObserverOptions = {
   rootMargin: '1000px',
 };
 
+const MAP_FIELDS_TO_STEPS = {
+    1: ['title', 'description', 'internalNotes'],
+    2: ['template'],
+    3: [],
+}
+
 const RenderBatchFlowFields: React.FC<Props> = (props) => {
 
   const {
