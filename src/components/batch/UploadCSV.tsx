55d39925affbbf4e7d5a325202f1aa48bc6341d4
goblincore <withallmyhrt@gmail.com>
2023-08-29 11:57:27 -0400

    parse csv into object

--- src/components/batch/UploadCSV.tsx
+++ src/components/batch/UploadCSV.tsx
@@ -1,51 +1,49 @@
-
 import React, { useEffect, useState } from "react";
+import Papa from "papaparse";
 
 const UploadCSV: React.FC = () => {
+  const [file, setFile] = useState();
+  const [data, setData] = useState();
 
-    const [file, setFile] = useState();
+  const fileReader = new FileReader();
 
-    const fileReader = new FileReader();
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-    const handleOnChange = (e) => {
-        setFile(e.target.files[0]);
-    };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.data);
+      },
+    });
+  };
 
-        if (file) {
-            fileReader.onload = function (event) {
-                const csvOutput = event.target.result;
-            };
+  return (
+    <div>
+      <p>Upload CSV File with earner information</p>
+      <form>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
 
-            fileReader.readAsText(file);
-        }
-    };
-
-    return(
-        <div>
-            <p>Upload CSV File with earner information</p>
-            <form>
-                <input
-                    type={"file"}
-                    id={"csvFileInput"}
-                    accept={".csv"}
-                    onChange={handleOnChange}
-                />
-
-                <button
-                    onClick={(e) => {
-                        handleOnSubmit(e);
-                    }}
-                >
-                    IMPORT CSV
-                </button>
-            </form>
-
-
-        </div>
-    )
-}
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
+        >
+          IMPORT CSV
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default UploadCSV;
