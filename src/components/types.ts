b20fa8ce6799ce80a9363dbe59813a3a40ff3139
goblincore <withallmyhrt@gmail.com>
2023-08-31 23:34:17 -0400

    Expose ListIndexProps and ListPreferences types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,10 +1,12 @@
 // Edit/save view props types
 import { Props } from 'payload/dist/admin/components/views/collections/Edit/types';
-
+import { ListIndexProps, ListPreferences } from 'payload/dist/admin/components/views/collections/List/types';
 // Render Field props types
 import { Props as RenderFieldProps } from 'payload/dist/admin/components/forms/RenderFields/types';
 
 export {
     Props,
     RenderFieldProps,
+    ListIndexProps,
+    ListPreferences
 }
