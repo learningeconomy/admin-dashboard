6d23fb6648998468a987fffd5668068a8d519215
goblincore <withallmyhrt@gmail.com>
2023-09-01 00:04:53 -0400

    expost ListCollectionProps type

--- src/components/types.ts
+++ src/components/types.ts
@@ -3,10 +3,12 @@ import { Props } from 'payload/dist/admin/components/views/collections/Edit/type
 import { ListIndexProps, ListPreferences } from 'payload/dist/admin/components/views/collections/List/types';
 // Render Field props types
 import { Props as RenderFieldProps } from 'payload/dist/admin/components/forms/RenderFields/types';
+import { Props as ListCollectionProps } from 'payload/dist/admin/components/views/collections/List/types';
 
 export {
     Props,
+    ListCollectionProps,
     RenderFieldProps,
     ListIndexProps,
-    ListPreferences
+    ListPreferences,
 }
