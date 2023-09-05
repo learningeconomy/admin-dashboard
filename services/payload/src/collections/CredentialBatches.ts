4919ccc35d2ed542484eba01d819d7a1ff646789
Taylor Beeston <beeston.taylor@gmail.com>
2023-09-05 12:20:29 -0700

    :recycle: Set up monorepo

---
+++ services/payload/src/collections/CredentialBatches.ts
@@ -0,0 +1,66 @@
+import { CollectionConfig } from 'payload/types';
+import BatchPageDescription from '../components/batch/BatchPageDescription';
+import CreateBatch from '../components/batch/CreateBatch';
+
+const CredentialsBatchesCollection: CollectionConfig = {
+    slug: 'credential-batch',
+    admin: {
+        defaultColumns: ['title', 'id', 'status'],
+        useAsTitle: 'title',
+        description: BatchPageDescription,
+        components: {
+            views: {
+                Edit: CreateBatch,
+            },
+        },
+    },
+    versions: {
+        drafts: {
+            autosave: true,
+        },
+    },
+    fields: [
+        {
+            name: 'title',
+            type: 'text',
+            required: true,
+            minLength: 3,
+            maxLength: 100,
+        },
+        {
+            name: 'description',
+            type: 'textarea',
+            required: false,
+            maxLength: 1000,
+        },
+        {
+            name: 'internalNotes',
+            type: 'textarea',
+            required: false,
+            maxLength: 1000,
+        },
+        {
+            name: 'status',
+            type: 'text',
+            required: true,
+            defaultValue: 'DRAFT',
+            admin: { hidden: true },
+        },
+        {
+            name: 'template',
+            type: 'relationship',
+            required: true,
+            relationTo: 'credential-template',
+            hasMany: false,
+        },
+        {
+            name: 'emailTemplate',
+            type: 'relationship',
+            required: true,
+            relationTo: 'email-template',
+            hasMany: false,
+        },
+    ],
+};
+
+export default CredentialsBatchesCollection;
---
+++ services/payload/src/collections/Credentials.ts
@@ -0,0 +1,48 @@
+import { CollectionConfig } from 'payload/types';
+import CreateBatch from '../components/batch/CreateBatch';
+
+const CredentialsCollection: CollectionConfig = {
+    slug: 'credential',
+    admin: {
+        defaultColumns: ['credentialName', 'id', 'status'],
+        useAsTitle: 'credentialName',
+    },
+    versions: {
+        drafts: true,
+    },
+    fields: [
+        {
+            name: 'credentialName',
+            type: 'text',
+        },
+        {
+            name: 'earnerName',
+            type: 'text',
+        },
+        {
+            name: 'emailAddress',
+            type: 'email',
+        },
+        {
+            name: 'extraFields',
+            type: 'json',
+            required: false,
+        },
+        {
+            name: 'status',
+            type: 'text',
+            required: true,
+            defaultValue: 'DRAFT',
+            admin: { hidden: true },
+        },
+        {
+            name: 'batch',
+            type: 'relationship',
+            required: true,
+            relationTo: 'credential-batch',
+            hasMany: false,
+        },
+    ],
+};
+
+export default CredentialsCollection;
