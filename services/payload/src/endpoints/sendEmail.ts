42792a5e72b55843d13697a64ce279044af1a490
goblincore <withallmyhrt@gmail.com>
2023-09-06 12:02:03 -0400

    Add test email endpoint

---
+++ services/payload/src/endpoints/sendEmail.ts
@@ -0,0 +1,29 @@
+import { PayloadHandler } from "payload/config";
+import { Forbidden } from "payload/errors";
+import payload from "payload";
+
+export const sendEmail: PayloadHandler = async (req, res, next) => {
+  if (!req.user) throw new Forbidden();
+   console.log('////req?.body', req.body);
+  const query = {
+    batch: {
+      equals: req?.body?.message
+    }
+  }
+
+  try {
+    console.log("//req body", req?.body);
+    const data = await payload.sendEmail({
+        to: 'withallmyhrt@gmail.com',
+        subject: 'test email payload',
+        email: 'test email',
+    })
+   
+    console.log('///data found', data);
+
+    res.status(200).json({ data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
