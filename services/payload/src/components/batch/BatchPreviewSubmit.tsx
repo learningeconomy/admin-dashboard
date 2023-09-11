5c5432cb175c5fa13a78958a56eaf6431f18c05e
goblincore <withallmyhrt@gmail.com>
2023-09-11 18:41:08 -0400

    Add placeholder for last preview step of batch flow

---
+++ services/payload/src/components/batch/BatchPreviewSubmit.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+
+const BatchPreviewSubmit: React.FC = () => {
+
+
+
+
+
+
+    return (
+        <section>
+            <h1>Batch Preview</h1>
+
+
+
+
+
+        </section>
+    );
+}
+
+export default BatchPreviewSubmit;
