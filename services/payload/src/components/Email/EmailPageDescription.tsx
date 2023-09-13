31778c6691078fca1f72e41f23503a66fd0ca0d5
Rachel Hendricks <rachelhen92@gmail.com>
2023-09-13 11:39:57 -0400

    Add plus icon to buttons

--- services/payload/src/components/Email/EmailPageDescription.tsx
+++ services/payload/src/components/Email/EmailPageDescription.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useConfig } from "payload/dist/admin/components/utilities/Config";
 import { Link } from "react-router-dom";
+import './EmailPageDescription.scss';
 
 const EmailPageDescription: React.FC = () => {
   const {
@@ -14,7 +15,7 @@ const EmailPageDescription: React.FC = () => {
         activeClassName="active"
         to={`${adminRoute}/collections/email-template/create`}
       >
-        Create New Template
+        <img className="plus_icon" src="/assets/plus-icon.svg" alt="plus icon"/>Create New Template
       </Link>
     </div>
   );
--- services/payload/src/components/template/TemplatePageDescription.tsx
+++ services/payload/src/components/template/TemplatePageDescription.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useConfig } from "payload/dist/admin/components/utilities/Config";
 import { Link } from "react-router-dom";
+import './template.scss';
 
 const TemplatePageDescription: React.FC = () => {
   const {
@@ -14,7 +15,7 @@ const TemplatePageDescription: React.FC = () => {
         activeClassName="active"
         to={`${adminRoute}/collections/credential-template/create`}
       >
-        Create New Template
+        <img className="plus_icon" src="/assets/plus-icon.svg" alt="plus icon"/>Create New Template
       </Link>
     </div>
   );
