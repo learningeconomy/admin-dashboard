078c4782baa7054c52a0ae5f48e3c0b3d1ea8280
goblincore <withallmyhrt@gmail.com>
2023-09-20 10:40:24 -0400

    add steps

--- services/payload/src/endpoints/sendBatchEmail.ts
+++ services/payload/src/endpoints/sendBatchEmail.ts
@@ -2,17 +2,34 @@ import { PayloadHandler } from "payload/config";
 import { Forbidden } from "payload/errors";
 import payload from "payload";
 import { emailQueue } from "../jobs/queue.server";
+import { generateJwtFromId } from "../helpers/jwtHelpers";
 
 export const sendBatchEmail: PayloadHandler = async (req, res, next) => {
   if (!req.user) throw new Forbidden();
    console.log('////req?.body', req.body);
+  //get batch id
+   const batchId = req?.body?.batchId;
+
+   // get all credentials records associated with batchId
   const query = {
     batch: {
       equals: req?.body?.message
     }
   }
 
+  //get all emails from credential records
+  
+
+  //generate email link
+  // use id from credential record to generate jwt
+  const _id = 'test';
+
+  const jwt = await generateJwtFromId(_id);
+
+
   // test email
+
+  // get email template for batch
 
 
   try {
@@ -30,7 +47,7 @@ export const sendBatchEmail: PayloadHandler = async (req, res, next) => {
 
     //actual email
     // get credential details and interpolate into email template
-    // send email
+    // queue up email jobs
    
     console.log('///data found', data);
 
