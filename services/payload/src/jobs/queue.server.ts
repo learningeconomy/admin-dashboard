5f9f9842b25467666f0b587bb8e113e778072cba
Taylor Beeston <beeston.taylor@gmail.com>
2023-09-22 11:57:20 -0700

    :sparkles: Fix compose with Redis

--- services/payload/src/jobs/queue.server.ts
+++ services/payload/src/jobs/queue.server.ts
@@ -1,59 +1,54 @@
-import type { Processor } from "bullmq";
-import { QueueEvents } from "bullmq";
-import { Queue, Worker } from "bullmq";
-import payload from "payload";
+import type { Processor } from 'bullmq';
+import { QueueEvents } from 'bullmq';
+import { Queue, Worker } from 'bullmq';
+import payload from 'payload';
 
+const redisUrl = process.env.REDIS_URL ?? 'localhost';
+const redisPort = Number(process.env.REDIS_PORT ?? '6379');
 
 // redis settings...
 const connection = {
-  host: 'localhost',
-  port: 6379
-}
+    host: redisUrl,
+    port: redisPort,
+};
 
 type AugmentedQueue<T> = Queue<T> & {
-  events: QueueEvents
-}
+    events: QueueEvents;
+};
 type RegisteredQueue = {
-  queue: Queue
-  queueEvents: QueueEvents
-  worker: Worker
-}
+    queue: Queue;
+    queueEvents: QueueEvents;
+    worker: Worker;
+};
 declare global {
-  var __registeredQueues:
-    | Record<string, RegisteredQueue>
-    | undefined
+    var __registeredQueues: Record<string, RegisteredQueue> | undefined;
 }
-const registeredQueues =
-  global.__registeredQueues ||
-  (global.__registeredQueues = {})
+const registeredQueues = global.__registeredQueues || (global.__registeredQueues = {});
 /**
  *
  * @param name Unique name of the queue
  * @param processor
  */
-export function registerQueue<T>(
-  name: string,
-  processor: Processor<T>,
-) {
-  if (!registeredQueues[name]) {
-    const queue = new Queue(name, { connection })
-    const queueEvents = new QueueEvents(name, {
-      connection,
-    })
-    const worker = new Worker<T>(name, processor, {
-      connection,
-      lockDuration: 1000 * 60 * 15,
-      concurrency: 8,
-    })
-    registeredQueues[name] = {
-      queue,
-      queueEvents,
-      worker,
+export function registerQueue<T>(name: string, processor: Processor<T>) {
+    if (!registeredQueues[name]) {
+        const queue = new Queue(name, { connection });
+        const queueEvents = new QueueEvents(name, {
+            connection,
+        });
+        const worker = new Worker<T>(name, processor, {
+            connection,
+            lockDuration: 1000 * 60 * 15,
+            concurrency: 8,
+        });
+        registeredQueues[name] = {
+            queue,
+            queueEvents,
+            worker,
+        };
     }
-  }
-  const queue = registeredQueues[name].queue as AugmentedQueue<T>
-  queue.events = registeredQueues[name].queueEvents
-  return queue
+    const queue = registeredQueues[name].queue as AugmentedQueue<T>;
+    queue.events = registeredQueues[name].queueEvents;
+    return queue;
 }
 
 // This will run in the same thread as the main app
@@ -63,11 +58,8 @@ export function registerQueue<T>(
 the registerQueue function, BullMQ will spawn a new process to run the file. 
 These are called sandboxed processors."
 */
-export const emailQueue = registerQueue(
-  "email",
-  async (job: any) => {
+export const emailQueue = registerQueue('email', async (job: any) => {
     console.log('///emailQueue job', job);
-    const { to, subject, text, html } = job.data
-    await payload.sendEmail({to, subject, text, html})
-  },
-)
+    const { to, subject, text, html } = job.data;
+    await payload.sendEmail({ to, subject, text, html });
+});
