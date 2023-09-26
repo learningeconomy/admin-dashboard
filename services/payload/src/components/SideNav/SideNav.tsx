262d1733d601decde23171feb2e4d121d228f640
Rachel Hendricks <rachelhen92@gmail.com>
2023-09-26 16:57:35 -0400

    Change buttons to NavLinks and remove commented out code

--- services/payload/src/components/SideNav/SideNav.tsx
+++ services/payload/src/components/SideNav/SideNav.tsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './SideNav.scss';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useConfig } from 'payload/dist/admin/components/utilities/Config';
 import Logout from '../Logout/Logout';
 import Account from '../Account/Account';
 import { useAuth } from 'payload/dist/admin/components/utilities/Auth';
 
-
-import { Icon } from '../Icon';
-
 const SideNav: React.FC = () => {
-    const [activeButton, setActiveButton] = useState('');
-    // const [navbarOpen, setNavbarOpen] = useState(false);
-    const history = useHistory();
-    const baseClass = 'nav';
     const { user } = useAuth();
 
     const {
@@ -22,89 +15,42 @@ const SideNav: React.FC = () => {
         },
       } = useConfig();
 
-    const handleClick = (e) => {
-        history.push('')
-        if ((history?.location?.pathname.includes(`admin/account`))) {
-            setActiveButton('');
-        }
-        setActiveButton(e.currentTarget.firstChild.alt);
-        history.push(`admin/collections/${e.currentTarget.firstChild.alt}?limit=10`);
-    };
-
   return (
     <div className="navbar-wrapper">
-        {/* <button className="navbar-chevron-button" onClick={() => {setNavbarOpen(!navbarOpen)}}>
-            {!navbarOpen ? <><img src="/assets/chevron-left.svg" alt="chevron left"/><img className="chevron-right" src="/assets/chevron-left.svg" alt="chevron right"/></> :
-            <><img className="chevron-right navbar-open-chevron" src="/assets/chevron-left.svg" alt="chevron right"/><img src="/assets/chevron-left.svg" alt="chevron left"/></> }
-        </button> */}
-        {/* {!navbarOpen ?
-        <> 
-            <div className="navbar-icon-wrapper">
-                <Icon />
-            </div>
-            <div className="navbar-buttons-wrap">
-                <button className={activeButton === 'credential-batch' ? "navbar-buttons active" : "navbar-buttons"} onClick={handleClick}>
-                    <img src="/assets/list-checks.svg" alt="credential-batch"/>
-                </button>
-                <button className={activeButton === 'credential' ? "navbar-buttons active" : "navbar-buttons"}  onClick={handleClick}>
-                    <img src="/assets/file-check.svg" alt="credential"/>
-                </button>
-                <button className={activeButton === 'credential-template' ? "navbar-buttons active" : "navbar-buttons"} onClick={handleClick}>
-                    <img src="/assets/file-edit.svg" alt="credential-template"/>
-                </button>
-                <button className={activeButton === 'email-template' ? "navbar-buttons active" : "navbar-buttons"} onClick={handleClick}>
-                    <img src="/assets/mail-plus.svg" alt="email-template"/>
-                </button>
-            </div>
-            <div className='navbar-closed-user-wrapper'>
+        <div className="navbar-icon-wrapper">
+            <img src="/assets/tdm-alt-logo.png" alt="Tec de Monterray logo"/>
+        </div>
+        <div className="navbar-buttons-wrap">
+            <NavLink className="navbar-buttons" to="/admin/collections/credential-batch">
+                <img src="/assets/list-checks.svg" alt="credential-batch"/> Issuance Overview
+            </NavLink>
+            <NavLink className="navbar-buttons" to="/admin/collections/credential">
+                <img src="/assets/file-check.svg" alt="credential"/> Credentials
+            </NavLink>
+            <NavLink className="navbar-buttons" to="/admin/collections/credential-template">
+                <img src="/assets/file-edit.svg" alt="credential-template"/> Credential Templates
+            </NavLink>
+            <NavLink className="navbar-buttons" to="/admin/collections/email-template">
+                <img src="/assets/mail-plus.svg" alt="email-template"/> Email Templates
+            </NavLink>
+            <NavLink className="navbar-buttons" to="/admin/collections/users">
+                <img src="/assets/users.svg" alt="users"/> Users
+            </NavLink>
+        </div>
+        <div className="navbar-user-wrapper">
+            <div className="navbar-user">
                 <Link
                 to={`${admin}/account`}
-                className={`${baseClass}__account`}
-                onClick={handleClick}
+                className="nav__account"
                 >
-                <Account />
+                <Account /> 
+                <p>{user.email}</p>
                 </Link>
-                <Logout />
-            </div> 
-        </> : */}
-        <> 
-            <div className="navbar-open-icon-wrapper">
-                <img src="/assets/tdm-alt-logo.png" alt="Tec de Monterray logo"/>
             </div>
-            <div className="navbar-buttons-wrap">
-                <button className={activeButton === "credential-batch" ? "navbar-open-buttons active" : "navbar-open-buttons"} onClick={handleClick}>
-                    <img src="/assets/list-checks.svg" alt="credential-batch"/> Issuance Overview
-                </button>
-                <button className={activeButton === "credential" ? "navbar-open-buttons active" : "navbar-open-buttons"} onClick={handleClick}>
-                    <img src="/assets/file-check.svg" alt="credential"/> Credentials
-                </button>
-                <button className={activeButton === "credential-template" ? "navbar-open-buttons active" : "navbar-open-buttons"} onClick={handleClick}>
-                    <img src="/assets/file-edit.svg" alt="credential-template"/> Credential Templates
-                </button>
-                <button className={activeButton === "email-template" ? "navbar-open-buttons active" : "navbar-open-buttons"} onClick={handleClick}>
-                    <img src="/assets/mail-plus.svg" alt="email-template"/> Email Templates
-                </button>
-                <button className={activeButton === "users" ? "navbar-open-buttons active" : "navbar-open-buttons"} onClick={handleClick}>
-                    <img src="/assets/users.svg" alt="users"/> Users
-                </button>
+            <div className="navbar-logout-wrapper">
+                <Logout /> 
             </div>
-            <div className="navbar-open-user-wrapper">
-                <div className="navbar-open-user">
-                    <Link
-                    to={`${admin}/account`}
-                    className={`${baseClass}__account`}
-                    onClick={handleClick}
-                    >
-                    <Account /> 
-                    <p>{user.email}</p>
-                    </Link>
-                </div>
-                <div className="navbar-open-logout-wrapper">
-                    <Logout /> 
-                </div>
-            </div>   
-        </>
-        {/* } */}
+        </div>   
     </div>
   );
 };
