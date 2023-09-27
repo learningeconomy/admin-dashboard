6075ee4f0677518eb9d0008cd5dc5fec93a02c8a
Taylor Beeston <beeston.taylor@gmail.com>
2023-09-27 10:01:57 -0700

    :lipstick: Grace Feedback

--- services/payload/src/components/List/DefaultListView.tsx
+++ services/payload/src/components/List/DefaultListView.tsx
@@ -27,213 +27,181 @@ import './index.scss';
 
 const baseClass = 'collection-list';
 
-const DefaultListView: React.FC<Props> = (props) => {
-  const {
-    collection,
-    collection: {
-      labels: {
-        singular: singularLabel,
-        plural: pluralLabel,
-      },
-      admin: {
-        description,
-        components: {
-          BeforeList,
-          BeforeListTable,
-          AfterListTable,
-          AfterList,
-        } = {},
-      } = {},
-    },
-    data,
-    newDocumentURL,
-    limit,
-    hasCreatePermission,
-    disableEyebrow,
-    modifySearchParams,
-    handleSortChange,
-    handleWhereChange,
-    handlePageChange,
-    handlePerPageChange,
-    customHeader,
-    resetParams,
-  } = props;
+const DefaultListView: React.FC<Props> = props => {
+    const {
+        collection,
+        collection: {
+            labels: { singular: singularLabel, plural: pluralLabel },
+            admin: {
+                description,
+                components: { BeforeList, BeforeListTable, AfterListTable, AfterList } = {},
+            } = {},
+        },
+        data,
+        newDocumentURL,
+        limit,
+        hasCreatePermission,
+        disableEyebrow,
+        modifySearchParams,
+        handleSortChange,
+        handleWhereChange,
+        handlePageChange,
+        handlePerPageChange,
+        customHeader,
+        resetParams,
+    } = props;
 
-  const { breakpoints: { s: smallBreak } } = useWindowInfo();
-  const { t, i18n } = useTranslation('general');
-  let formattedDocs = data.docs || [];
+    const {
+        breakpoints: { s: smallBreak },
+    } = useWindowInfo();
+    const { t, i18n } = useTranslation('general');
+    let formattedDocs = data.docs || [];
 
-  if (collection.upload) {
-    formattedDocs = formattedDocs?.map((doc) => {
-      return {
-        ...doc,
-        filesize: formatFilesize(doc.filesize),
-      };
-    });
-  }
+    if (collection.upload) {
+        formattedDocs = formattedDocs?.map(doc => {
+            return {
+                ...doc,
+                filesize: formatFilesize(doc.filesize),
+            };
+        });
+    }
 
-  return (
-    <div className={baseClass}>
-      {Array.isArray(BeforeList) && BeforeList.map((Component, i) => (
-        <Component
-          key={i}
-          {...props}
-        />
-      ))}
+    return (
+        <div className={baseClass}>
+            {Array.isArray(BeforeList) &&
+                BeforeList.map((Component, i) => <Component key={i} {...props} />)}
 
-      <Meta
-        title={getTranslation(collection.labels.plural, i18n)}
-      />
-      <SelectionProvider
-        docs={data.docs}
-        totalDocs={data.totalDocs}
-      >
-        {!disableEyebrow && (
-          <Eyebrow />
-        )}
-        <Gutter className={`${baseClass}__wrap`}>
-          <header className={`${baseClass}__header`}>
-            {customHeader && customHeader}
-            {!customHeader && (
-              <Fragment>
-                <h1>
-                  {getTranslation(pluralLabel, i18n)}
-                </h1>
-                {hasCreatePermission && (
-                  <Pill to={newDocumentURL}>
-                    {t('createNew')}
-                  </Pill>
-                )}
-                {!smallBreak && (
-                  <ListSelection
-                    label={getTranslation(collection.labels.plural, i18n)}
-                  />
-                )}
-                {description && (
-                  <div className={`${baseClass}__sub-header`}>
-                    <ViewDescription description={description} />
-                  </div>
-                )}
-              </Fragment>
-            )}
-          </header>
-          <ListControls
-            collection={collection}
-            modifySearchQuery={modifySearchParams}
-            handleSortChange={handleSortChange}
-            handleWhereChange={handleWhereChange}
-            resetParams={resetParams}
-          />
-          {Array.isArray(BeforeListTable) && BeforeListTable.map((Component, i) => (
-            <Component
-              key={i}
-              {...props}
-            />
-          ))}
-          {!data.docs && (
-            <StaggeredShimmers
-              className={[`${baseClass}__shimmer`, `${baseClass}__shimmer--rows`].join(' ')}
-              count={6}
-            />
-          )}
-          {(data.docs && data.docs.length > 0) && (
-            <RelationshipProvider>
-              <Table data={formattedDocs} />
-            </RelationshipProvider>
-          )}
-          {data.docs && data.docs.length === 0 && (
-            <div className={`${baseClass}__no-results`}>
-              <p>
-                {t('noResults', { label: getTranslation(pluralLabel, i18n) })}
-              </p>
-              {hasCreatePermission && newDocumentURL && (
-                <Button
-                  el="link"
-                  to={newDocumentURL}
-                >
-                  {t('createNewLabel', { label: getTranslation(singularLabel, i18n) })}
-                </Button>
-              )}
-            </div>
-          )}
-          {Array.isArray(AfterListTable) && AfterListTable.map((Component, i) => (
-            <Component
-              key={i}
-              {...props}
-            />
-          ))}
-
-          <div className={`${baseClass}__page-controls`}>
-            <Paginator
-              limit={data.limit}
-              totalPages={data.totalPages}
-              page={data.page}
-              hasPrevPage={data.hasPrevPage}
-              hasNextPage={data.hasNextPage}
-              prevPage={data.prevPage}
-              nextPage={data.nextPage}
-              numberOfNeighbors={1}
-              disableHistoryChange={modifySearchParams === false}
-              onChange={handlePageChange}
-            />
-            {data?.totalDocs > 0 && (
-              <Fragment>
-                <div className={`${baseClass}__page-info`}>
-                  {(data.page * data.limit) - (data.limit - 1)}
-                  -
-                  {data.totalPages > 1 && data.totalPages !== data.page ? (data.limit * data.page) : data.totalDocs}
-                  {' '}
-                  {t('of')}
-                  {' '}
-                  {data.totalDocs}
-                </div>
-                <PerPage
-                  limits={collection?.admin?.pagination?.limits}
-                  limit={limit}
-                  modifySearchParams={modifySearchParams}
-                  handleChange={handlePerPageChange}
-                  resetPage={data.totalDocs <= data.pagingCounter}
-                />
-                <div className={`${baseClass}__list-selection`}>
-                  {smallBreak && (
-                    <Fragment>
-                      <ListSelection
-                        label={getTranslation(collection.labels.plural, i18n)}
-                      />
-                      <div className={`${baseClass}__list-selection-actions`}>
-                        <EditMany
-                          collection={collection}
-                          resetParams={resetParams}
+            <Meta title={getTranslation(collection.labels.plural, i18n)} />
+            <SelectionProvider docs={data.docs} totalDocs={data.totalDocs}>
+                {!disableEyebrow && <Eyebrow />}
+                <Gutter className={`${baseClass}__wrap`}>
+                    <header className={`${baseClass}__header`}>
+                        {customHeader && customHeader}
+                        {!customHeader && (
+                            <Fragment>
+                                <h1>{getTranslation(pluralLabel, i18n)}</h1>
+                                {hasCreatePermission && (
+                                    <Pill to={newDocumentURL}>{t('createNew')}</Pill>
+                                )}
+                                {!smallBreak && (
+                                    <ListSelection
+                                        label={getTranslation(collection.labels.plural, i18n)}
+                                    />
+                                )}
+                                {description && (
+                                    <div className={`${baseClass}__sub-header`}>
+                                        <ViewDescription description={description} />
+                                    </div>
+                                )}
+                            </Fragment>
+                        )}
+                    </header>
+                    <ListControls
+                        collection={collection}
+                        modifySearchQuery={modifySearchParams}
+                        handleSortChange={handleSortChange}
+                        handleWhereChange={handleWhereChange}
+                        resetParams={resetParams}
+                    />
+                    {Array.isArray(BeforeListTable) &&
+                        BeforeListTable.map((Component, i) => <Component key={i} {...props} />)}
+                    {!data.docs && (
+                        <StaggeredShimmers
+                            className={[
+                                `${baseClass}__shimmer`,
+                                `${baseClass}__shimmer--rows`,
+                            ].join(' ')}
+                            count={6}
                         />
-                        <PublishMany
-                          collection={collection}
-                          resetParams={resetParams}
+                    )}
+                    {data.docs && data.docs.length > 0 && (
+                        <RelationshipProvider>
+                            <Table data={formattedDocs} />
+                        </RelationshipProvider>
+                    )}
+                    {data.docs && data.docs.length === 0 && (
+                        <div className={`${baseClass}__no-results`}>
+                            <p>{t('noResults', { label: getTranslation(pluralLabel, i18n) })}</p>
+                            {hasCreatePermission && newDocumentURL && (
+                                <Button el="link" to={newDocumentURL}>
+                                    {t('createNewLabel', {
+                                        label: getTranslation(singularLabel, i18n),
+                                    })}
+                                </Button>
+                            )}
+                        </div>
+                    )}
+                    {Array.isArray(AfterListTable) &&
+                        AfterListTable.map((Component, i) => <Component key={i} {...props} />)}
+
+                    <div className={`${baseClass}__page-controls`}>
+                        <Paginator
+                            limit={data.limit}
+                            totalPages={data.totalPages}
+                            page={data.page}
+                            hasPrevPage={data.hasPrevPage}
+                            hasNextPage={data.hasNextPage}
+                            prevPage={data.prevPage}
+                            nextPage={data.nextPage}
+                            numberOfNeighbors={1}
+                            disableHistoryChange={modifySearchParams === false}
+                            onChange={handlePageChange}
                         />
-                        <UnpublishMany
-                          collection={collection}
-                          resetParams={resetParams}
-                        />
-                        <DeleteMany
-                          collection={collection}
-                          resetParams={resetParams}
-                        />
-                      </div>
-                    </Fragment>
-                  )}
-                </div>
-              </Fragment>
-            )}
-          </div>
-        </Gutter>
-      </SelectionProvider>
-      {Array.isArray(AfterList) && AfterList.map((Component, i) => (
-        <Component
-          key={i}
-          {...props}
-        />
-      ))}
-    </div>
-  );
+                        {data?.totalDocs > 0 && (
+                            <Fragment>
+                                <div className={`${baseClass}__page-info`}>
+                                    {data.page * data.limit - (data.limit - 1)}-
+                                    {data.totalPages > 1 && data.totalPages !== data.page
+                                        ? data.limit * data.page
+                                        : data.totalDocs}{' '}
+                                    {t('of')} {data.totalDocs}
+                                </div>
+                                <PerPage
+                                    limits={collection?.admin?.pagination?.limits}
+                                    limit={limit}
+                                    modifySearchParams={modifySearchParams}
+                                    handleChange={handlePerPageChange}
+                                    resetPage={data.totalDocs <= data.pagingCounter}
+                                />
+                                <div className={`${baseClass}__list-selection`}>
+                                    {smallBreak && (
+                                        <Fragment>
+                                            <ListSelection
+                                                label={getTranslation(
+                                                    collection.labels.plural,
+                                                    i18n
+                                                )}
+                                            />
+                                            <div className={`${baseClass}__list-selection-actions`}>
+                                                <EditMany
+                                                    collection={collection}
+                                                    resetParams={resetParams}
+                                                />
+                                                <PublishMany
+                                                    collection={collection}
+                                                    resetParams={resetParams}
+                                                />
+                                                <UnpublishMany
+                                                    collection={collection}
+                                                    resetParams={resetParams}
+                                                />
+                                                <DeleteMany
+                                                    collection={collection}
+                                                    resetParams={resetParams}
+                                                />
+                                            </div>
+                                        </Fragment>
+                                    )}
+                                </div>
+                            </Fragment>
+                        )}
+                    </div>
+                </Gutter>
+            </SelectionProvider>
+            {Array.isArray(AfterList) &&
+                AfterList.map((Component, i) => <Component key={i} {...props} />)}
+        </div>
+    );
 };
 
 export default DefaultListView;
--- services/payload/src/components/credential/CredentialStatusCell.tsx
+++ services/payload/src/components/credential/CredentialStatusCell.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Props } from 'payload/components/views/Cell';
+import { capitalize } from '../../helpers/string.helpers';
 
 const CredentialStatusCell: React.FC<Props> = ({ rowData }) => {
     return (
         <section className="credential-status">
             <span className={(rowData.status as string).toLowerCase()}>
-                {rowData.status as string}
+                {capitalize(rowData.status as string)}
             </span>
         </section>
     );
