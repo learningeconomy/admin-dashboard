741b62792a7e0b1545d0be6d83ca3ffb824e2473
Taylor Beeston <beeston.taylor@gmail.com>
2023-10-17 13:28:39 -0700

    :bug: Bump session to 1 week
    fixes 13

--- services/payload/src/collections/Users.ts
+++ services/payload/src/collections/Users.ts
@@ -3,7 +3,7 @@ import UserPageDescription from '../components/User/UserPageDescription';
 
 const Users: CollectionConfig = {
     slug: 'users',
-    auth: true,
+    auth: { tokenExpiration: 7 * 24 * 60 * 60 },
     admin: { useAsTitle: 'name', description: UserPageDescription },
     fields: [{ name: 'name', type: 'text' }],
 };
