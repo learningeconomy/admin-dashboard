7beec0e5a6ddb63cb04ccb010ba0d3dd273d50dc
goblincore <withallmyhrt@gmail.com>
2023-11-09 14:48:15 -0500

    Add objectwithoutKey helper

--- services/payload/src/helpers/credential.helpers.ts
+++ services/payload/src/helpers/credential.helpers.ts
@@ -1,1 +1,6 @@
 export const AUTOMATIC_FIELDS = ['earnerName', 'credentialName', 'emailAddress', 'now'];
+
+export const objectWithoutKey = (object, key) => {
+    const { [key]: deletedKey, ...otherKeys } = object;
+    return { otherKeys, deletedKey };
+};
