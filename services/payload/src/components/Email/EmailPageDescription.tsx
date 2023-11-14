4011d5a23317669e12f2aa28a8f9042c54600874
goblincore <withallmyhrt@gmail.com>
2023-11-14 14:23:33 -0500

    Update email collection page count

--- services/payload/src/components/Email/EmailPageDescription.tsx
+++ services/payload/src/components/Email/EmailPageDescription.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useConfig } from 'payload/dist/admin/components/utilities/Config';
 import { Link } from 'react-router-dom';
 import './EmailPageDescription.scss';
+import { totalEmailTemplatePublishedQuery } from '../../constants/countQueries';
 
 const EmailPageDescription: React.FC = () => {
     const {
         routes: { admin: adminRoute },
     } = useConfig();
 
+    const [count, setCount] = useState<number | undefined>(0);
+
+    const fetchBatchCredentials = async (page = 1) => {
+        const res = await fetch('/api/get-collection-count', {
+            method: 'POST',
+            body: JSON.stringify({
+                collectionName: 'email-template',
+                query: totalEmailTemplatePublishedQuery,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+
+        if (res.status === 200) {
+            const data = await res.json();
+
+            setCount(data?.count);
+        }
+    };
+
+    useEffect(() => {
+        fetchBatchCredentials();
+    }, []);
+
     return (
         <div className="header_wrapper">
             <p className="header_paragraph">
-                <span className="header_number">2</span> Published Templates
+                <span className="header_number">{count}</span> Published Templates
             </p>
             <Link
                 className="header_template_button"
