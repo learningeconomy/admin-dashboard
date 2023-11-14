f940a1662b55afd403cb62e6803f94343247de68
Taylor Beeston <beeston.taylor@gmail.com>
2023-11-14 11:44:30 -0800

    :zap: Use count for getting counts

--- services/payload/src/constants/countQueries.ts
+++ services/payload/src/constants/countQueries.ts
@@ -1,18 +1,9 @@
 import { CREDENTIAL_BATCH_STATUS } from '../constants/batches';
 
-
 // payload localAPI query object for getting credential batches that are of sent status
-export const totalSentBatchesQuery = { status: { equals: CREDENTIAL_BATCH_STATUS.SENT } };
+export const totalSentBatchesQuery = { status: CREDENTIAL_BATCH_STATUS.SENT };
 
 //payload localAPI query object for getting all credential templates that are published status
-export const totalCredentialTemplatePublishedQuery = {
-    title: {
-        exists: true
-    },
-};
+export const totalCredentialTemplatePublishedQuery = { title: { $exists: true } };
 
-export const totalEmailTemplatePublishedQuery = {
-    title: {
-        exists: true
-    },
-}
+export const totalEmailTemplatePublishedQuery = { title: { $exists: true } };
