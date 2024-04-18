b0d60f1fe317926a1b232279b39a4c27072412bf
Taylor Beeston <beeston.taylor@gmail.com>
2024-04-18 12:58:36 -0700

    :goal_net: Catch weird did error

--- services/payload/src/helpers/learncard.helpers.ts
+++ services/payload/src/helpers/learncard.helpers.ts
@@ -9,18 +9,23 @@ export const getLearnCard = async () => {
 };
 
 export const areDidsEqual = async (did1: string, did2: string) => {
-    if (did1 === did2) return true;
+    try {
+        if (did1 === did2) return true;
 
-    const lc = await getLearnCard();
+        const lc = await getLearnCard();
 
-    const [resolvedDid1, resolvedDid2] = await Promise.all([
-        lc.invoke.resolveDid(did1),
-        lc.invoke.resolveDid(did2),
-    ]);
+        const [resolvedDid1, resolvedDid2] = await Promise.all([
+            lc.invoke.resolveDid(did1),
+            lc.invoke.resolveDid(did2),
+        ]);
 
-    return resolvedDid1?.verificationMethod?.some(method1 => {
-        return resolvedDid2?.verificationMethod?.some(
-            method2 => method1?.publicKeyJwk?.x === method2?.publicKeyJwk?.x
-        );
-    });
+        return resolvedDid1?.verificationMethod?.some(method1 => {
+            return resolvedDid2?.verificationMethod?.some(
+                method2 => method1?.publicKeyJwk?.x === method2?.publicKeyJwk?.x
+            );
+        });
+    } catch (error) {
+        console.error('Are dids equal error', error);
+        return false;
+    }
 };
