5c37e1e25dd169925dec3833bcb6538327cac3ff
Jacksón Smith <custard7@gmail.com>
2024-05-14 15:30:15 -0400

    Fix exchange endpoint

--- services/payload/src/helpers/issuerCoordinator/internal/exchange.ts
+++ services/payload/src/helpers/issuerCoordinator/internal/exchange.ts
@@ -10,93 +10,100 @@ import { CREDENTIAL_STATUS } from '../../../constants/credentials';
 import { getDelCredentialIdForChallenge } from './challenges';
 
 const exchange = async (
-	req: PayloadRequest, 
+	req: PayloadRequest,
 	collection: string,
 	credentialId: string,
 	retrievalId: string,
 	challenge: string,
 	didAuthVP: VP
 ): Promise<number | VC> => {
-	const learnCard = await getLearnCard();
+	try {
+		const learnCard = await getLearnCard();
 
-	// Verify DID-Auth VP
-	const result = await learnCard.invoke.verifyPresentation(didAuthVP);
+		// Verify DID-Auth VP
+		const result = await learnCard.invoke.verifyPresentation(didAuthVP);
 
-	// If invalid, return forbidden status
-	if (result.errors.length > 0) {
-		return 401;
-	}
-
-	const did = didAuthVP.holder;
-
-	const credentialIdStoredForChallenge = await getDelCredentialIdForChallenge(
-		didAuthVP?.proof?.challenge
-	);
-
- 	// If challenge fails, return forbidden status
-	if (!credentialIdStoredForChallenge || !(credentialIdStoredForChallenge === credentialId)) {
-		return 401;
-	}
-
-	const credential = await payload.findByID({ id: credentialId, collection, depth: 3 });
-
-	if (
-		typeof credential?.batch === 'string' ||
-		typeof credential.batch.template === 'string' ||
-		!credential.batch.template.credentialTemplateJson
-	) {
-		return 404;
-	}
-
-	let builtCredential = insertValuesIntoHandlebarsJsonTemplate(
-		JSON.stringify(credential.batch.template.credentialTemplateJson),
-		{
-			...(inflateObject as any)(credential.extraFields as any),
-			credentialName: credential.credentialName,
-			earnerName: credential.earnerName,
-			emailAddress: credential.emailAddress,
-			now: new Date().toISOString(),
-			issuanceDate: new Date().toISOString(),
+		// If invalid, return forbidden status
+		if (result.errors.length > 0) {
+			return 401;
 		}
-	) as any as UnsignedVC;
 
-	// Prep for sending to signing service
-	builtCredential.id = credential.id;
-	if (typeof builtCredential?.issuer === 'string') builtCredential.issuer = {};
-	builtCredential.issuer.id = learnCard.id.did();
+		const did = didAuthVP.holder;
 
-	if (!builtCredential.credentialSubject) builtCredential.credentialSubject = {};
-	if (Array.isArray(builtCredential.credentialSubject)) {
-		builtCredential.credentialSubject = builtCredential.credentialSubject.map(subject => ({
-			...subject,
-			id: did,
-		}));
-	} else {
-		builtCredential.credentialSubject.id = did;
+		const credentialIdStoredForChallenge = await getDelCredentialIdForChallenge(
+			didAuthVP?.proof?.challenge
+		);
+
+		// If challenge fails, return forbidden status
+		if (!credentialIdStoredForChallenge || !(credentialIdStoredForChallenge === credentialId)) {
+			return 401;
+		}
+
+		const credential = await payload.findByID({ id: credentialId, collection, depth: 3 });
+
+		if (
+			typeof credential?.batch === 'string' ||
+			typeof credential.batch.template === 'string' ||
+			!credential.batch.template.credentialTemplateJson
+		) {
+			return 404;
+		}
+
+		let builtCredential = insertValuesIntoHandlebarsJsonTemplate(
+			JSON.stringify(credential.batch.template.credentialTemplateJson),
+			{
+				...(inflateObject as any)(credential.extraFields as any),
+				credentialName: credential.credentialName,
+				earnerName: credential.earnerName,
+				emailAddress: credential.emailAddress,
+				now: new Date().toISOString(),
+				issuanceDate: new Date().toISOString(),
+			}
+		) as any as UnsignedVC;
+
+		// Prep for sending to signing service
+		builtCredential.id = credential.id;
+		if (typeof builtCredential?.issuer === 'string') builtCredential.issuer = {};
+		builtCredential.issuer.id = learnCard.id.did();
+
+		if (!builtCredential.credentialSubject) builtCredential.credentialSubject = {};
+		if (Array.isArray(builtCredential.credentialSubject)) {
+			builtCredential.credentialSubject = builtCredential.credentialSubject.map(subject => ({
+				...subject,
+				id: did,
+			}));
+		} else {
+			builtCredential.credentialSubject.id = did;
+		}
+
+		if (!builtCredential.issuanceDate) {
+			builtCredential.issuanceDate = new Date().toISOString();
+		}
+
+		// TODO: Add Status List revocation into credential
+
+		// Sign VC
+		const issuedCredential = await learnCard.invoke.issueCredential(builtCredential);
+
+		// Update VC Status to claimed
+		await payload.update({
+			collection,
+			id: credentialId,
+			data: {
+				status: CREDENTIAL_STATUS.CLAIMED,
+				...(collection === 'membership'
+					? { targetDid: builtCredential?.credentialSubject?.id }
+					: {}),
+			},
+			req,
+		});
+
+		// Return Credential
+		return issuedCredential;
+	} catch (e) {
+		console.error(e);
+		return 500;
 	}
-
-	if (!builtCredential.issuanceDate) {
-		builtCredential.issuanceDate = new Date().toISOString();
-	}
-
-	// TODO: Add Status List revocation into credential
-
-	// Sign VC
-	const issuedCredential = await learnCard.invoke.issueCredential(builtCredential);
-
-	// Update VC Status to claimed
-	await payload.update({
-		collection,
-		id: credentialId,
-		data: {
-			status: CREDENTIAL_STATUS.CLAIMED,
-			...(collection === 'membership' ? { targetDid: credential.credentialSubject.id } : {}),
-		},
-		req
-	});
-
-	// Return Credential
-	return issuedCredential;
 };
 
 export default exchange;
